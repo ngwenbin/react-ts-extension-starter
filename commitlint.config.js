e04d3424fe57ab27445bdc40cd5d37f504bc625a
Wen Bin <wenbin_ng@yahoo.com.sg>
2023-10-23 23:14:16 +0800

    chore: update config files

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "chore",
-        "feat",
-        "fix",
-        "change",
-        "docs",
-        "refactor",
-        "test",
-        "build",
-        "ci",
-        "revert",
-      ],
-    ],
-  },
 };
