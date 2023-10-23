3dac42a24a60c1a24550b5ba6b99efb781fd317c
Wen Bin <wenbin_ng@yahoo.com.sg>
2023-10-23 12:06:43 +0800

    chore: added options page

--- src/pages/options/App.tsx
+++ src/pages/options/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = (): JSX.Element => {
   return (
     <div>
-      <h1>Options Page</h1>
+      <h1>Popup Page</h1>
       <p>If you are seeing this, React is working!</p>
     </div>
   );
